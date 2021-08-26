3472fab8f586f28d58302913400fd5c99ed3234f
satoruosada <satoru.5714@gmail.com>
2021-08-26 14:57:14 +0900

    パララックス対応

--- dist/js/main.js
+++ dist/js/main.js
@@ -52,18 +52,18 @@ document.addEventListener("DOMContentLoaded", function () {
 $(function () {
   var ua = navigator.userAgent.toLowerCase(); // alert(ua);
 
-  if (ua.indexOf('iphone') > -1 || ua.indexOf('android') > -1 && ua.indexOf('mobile') > -1) {
+  if (ua.indexOf("iphone") > -1 || ua.indexOf("android") > -1 && ua.indexOf("mobile") > -1) {
     // スマートフォン
     // alert('スマートフォン');
-    $('body').addClass('is-ios');
-  } else if (ua.indexOf('ipad') > -1 || ua.indexOf('macintosh') > -1 && 'ontouchend' in document) {
+    $("body").addClass("is-ios");
+  } else if (ua.indexOf("ipad") > -1 || ua.indexOf("macintosh") > -1 && "ontouchend" in document) {
     // タブレット
     // alert('タブレット');
-    $('body').addClass('is-ios');
+    $("body").addClass("is-ios");
   } else {
     // PC
     // alert('PC');
-    $('body').removeClass('is-ios');
+    $("body").removeClass("is-ios");
   }
 }); // ヒーロータイトル
 // var Emblem = {
@@ -107,126 +107,112 @@ $(function () {
 // };
 // Emblem.init('.loading__title');
 
-ScrollReveal().reveal('.fadeIn', {
+ScrollReveal().reveal(".work__images", {
   delay: 300,
   // アニメーション開始までの時間
   duration: 2000,
   // アニメーションの完了にかかる時間
   viewFactor: 0,
   // 0~1,どれくらい見えたら実行するか
-  origin: 'top',
+  origin: "right",
   // 要素がどの方向から来るか
-  opacity: 0,
-  //アニメーション開始時の透明度
-  reset: false // 何回もアニメーション表示するか
-
-});
-ScrollReveal().reveal('.work__images', {
-  delay: 300,
-  // アニメーション開始までの時間
-  duration: 2000,
-  // アニメーションの完了にかかる時間
-  viewFactor: 0,
-  // 0~1,どれくらい見えたら実行するか
-  origin: 'right',
-  // 要素がどの方向から来るか
-  distance: '50px',
+  distance: "50px",
   // 移動する距離
   reset: false // 何回もアニメーション表示するか
 
 });
-ScrollReveal().reveal('.about__images', {
+ScrollReveal().reveal(".about__images", {
   delay: 300,
   // アニメーション開始までの時間
   duration: 2000,
   // アニメーションの完了にかかる時間
   viewFactor: 0,
   // 0~1,どれくらい見えたら実行するか
-  origin: 'left',
+  origin: "left",
   // 要素がどの方向から来るか
-  distance: '50px',
+  distance: "50px",
   // 移動する距離
   reset: false // 何回もアニメーション表示するか
 
 });
-ScrollReveal().reveal('.staff__images-right', {
+ScrollReveal().reveal(".staff__images-right", {
   delay: 300,
   // アニメーション開始までの時間
   duration: 2000,
   // アニメーションの完了にかかる時間
   viewFactor: 0,
   // 0~1,どれくらい見えたら実行するか
-  origin: 'right',
+  origin: "right",
   // 要素がどの方向から来るか
-  distance: '50px',
+  distance: "50px",
   // 移動する距離
   reset: false // 何回もアニメーション表示するか
 
 });
-ScrollReveal().reveal('.staff__images-left', {
+ScrollReveal().reveal(".staff__images-left", {
   delay: 300,
   // アニメーション開始までの時間
   duration: 2000,
   // アニメーションの完了にかかる時間
   viewFactor: 0,
   // 0~1,どれくらい見えたら実行するか
-  origin: 'left',
+  origin: "left",
   // 要素がどの方向から来るか
-  distance: '50px',
+  distance: "50px",
   // 移動する距離
   reset: false // 何回もアニメーション表示するか
 
 });
-ScrollReveal().reveal('.work__person01', {
+ScrollReveal().reveal(".work__person01", {
   delay: 300,
   // アニメーション開始までの時間
   duration: 1000,
   // アニメーション完了にかかる時間
-  origin: 'top',
+  origin: "top",
   // 要素がどの方向から来るか
-  distance: '50px',
+  distance: "50px",
   // 移動する距離
   reset: false // フレームインの度に動かすか
 
 });
-ScrollReveal().reveal('.work__person02', {
+ScrollReveal().reveal(".work__person02", {
   delay: 300,
   // アニメーション開始までの時間
   duration: 1000,
   // アニメーション完了にかかる時間
-  origin: 'top',
+  origin: "top",
   // 要素がどの方向から来るか
-  distance: '50px',
+  distance: "50px",
   // 移動する距離
   reset: false // フレームインの度に動かすか
 
 });
-ScrollReveal().reveal('.about__person03', {
+ScrollReveal().reveal(".about__person03", {
   delay: 300,
   // アニメーション開始までの時間
   duration: 1000,
   // アニメーション完了にかかる時間
-  origin: 'top',
+  origin: "top",
   // 要素がどの方向から来るか
-  distance: '50px',
+  distance: "50px",
   // 移動する距離
   reset: false // フレームインの度に動かすか
 
 });
-ScrollReveal().reveal('.staff__person04', {
+ScrollReveal().reveal(".staff__person04", {
   delay: 200,
   // アニメーション開始までの時間
   duration: 1000,
   // アニメーション完了にかかる時間
-  origin: 'top',
+  origin: "top",
   // 要素がどの方向から来るか
-  distance: '70px',
+  distance: "70px",
   // 移動する距離
   reset: false // フレームインの度に動かすか
 
 }); // アニメーションさせたいクラス
 
-var container = $(".hero__title"); // アニメーションスピード
+var container = $(".loading__title"); // アニメーションスピード
 
 var speed = 80; // テキストの間にスペースを入れます
 
@@ -235,7 +221,7 @@ var text = $.trim(content);
 var newHtml = ""; // スペースで区切ったテキストを、テキストの数だけspanで囲む
 
 text.split("").forEach(function (v) {
-  newHtml += '<span>' + v + '</span>';
+  newHtml += "<span>" + v + "</span>";
 }); // spanで囲んだテキスト群をHTMLに戻す
 
 container.html(newHtml); // 1文字ずつ表示
@@ -245,25 +231,13 @@ container.css({
   opacity: 1
 });
 setInterval(function () {
-  container.find('span').eq(txtNum).css({
+  container.find("span").eq(txtNum).css({
     opacity: 1
   });
   txtNum++;
-}, speed); // フッタートップスクロール
-// $(function($){
-//   var sideBar = $('.footer__scroll > a');
-//   sideBar.hide();
-//   $(window).scroll(function () {
-//       if ($(this).scrollTop() > 100) {
-//         sideBar.fadeIn();
-//       } else {
-//         sideBar.fadeOut();
-//       }
-//   });
-// });
-
+}, speed);
 $(function () {
-  var pagetop = $('.footer__scroll > a'); // ボタン非表示
+  var pagetop = $(".footer__scroll > a"); // ボタン非表示
 
   pagetop.hide(); // 100px スクロールしたらボタン表示
 
@@ -275,33 +249,36 @@ $(function () {
     }
   });
   pagetop.click(function () {
-    $('body, html').animate({
+    $("body, html").animate({
       scrollTop: 0
     }, 500);
     return false;
   });
-}); //logoの表示
+}); // logoの表示
 
-$(window).on('load', function () {
-  $(".loading").delay(1800).fadeOut('slow'); //ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+$(window).on("load", function () {
+  $(".loading").delay(1600).fadeOut("200");
 }); // アーチ型ヒーロータイトル
 
 $(function () {
   $(".hero__title").arctext({
-    radius: 160,
-    dir: 1,
-    rotate: true
+    radius: 160
+  });
+  $(".loading__title").arctext({
+    radius: 160
   });
 }); // ロゴのフェイドイン
 
 $(function () {
   setTimeout(function () {
-    $('.hero__title-logo, .hero__title-logo2').fadeIn(1800);
-  }, 400); //1.9秒後にロゴをフェードイン
+    $(".hero__title, .hero__title-logo, .hero__title-logo2, .hero__title-logo3").fadeIn(800);
+  }, 2300); //1.9秒後にロゴをフェードイン
 });
-$(function () {
-  setTimeout(function () {
-    $('.hero__title-logo3').fadeIn(500);
-  }, 2200); //1.9秒後にロゴをフェードイン
+$('.loading__title01').fadeIn(1500);
+$('.loading__title02').fadeIn(1500); // パララックス
+
+new BackgroundVideo('.bv-video', {
+  src: [//動画保存先のパス
+  './images/top-movie.mp4']
 });
 //# sourceMappingURL=main.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -55,23 +55,26 @@ document.addEventListener("DOMContentLoaded", function () {
 $(function () {
   const ua = navigator.userAgent.toLowerCase();
   // alert(ua);
-  if (ua.indexOf('iphone') > -1 || (ua.indexOf('android') > -1 && ua.indexOf('mobile') > -1)) {
-      // スマートフォン
-      // alert('スマートフォン');
-      $('body').addClass('is-ios');
-
-  } else if (ua.indexOf('ipad') > -1 || ua.indexOf('macintosh') > -1 && 'ontouchend' in document) {
-      // タブレット
-      // alert('タブレット');
-      $('body').addClass('is-ios');
+  if (
+    ua.indexOf("iphone") > -1 ||
+    (ua.indexOf("android") > -1 && ua.indexOf("mobile") > -1)
+  ) {
+    // スマートフォン
+    // alert('スマートフォン');
+    $("body").addClass("is-ios");
+  } else if (
+    ua.indexOf("ipad") > -1 ||
+    (ua.indexOf("macintosh") > -1 && "ontouchend" in document)
+  ) {
+    // タブレット
+    // alert('タブレット');
+    $("body").addClass("is-ios");
   } else {
-      // PC
-      // alert('PC');
-      $('body').removeClass('is-ios');
+    // PC
+    // alert('PC');
+    $("body").removeClass("is-ios");
   }
-})
-
-
+});
 
 // ヒーロータイトル
 // var Emblem = {
@@ -96,7 +99,6 @@ $(function () {
 
 // Emblem.init('.hero__title');
 
-
 // var Emblem = {
 //   init: function(el, str) {
 //     var element = document.querySelector(el);
@@ -120,82 +122,71 @@ $(function () {
 // Emblem.init('.loading__title');
 
 
-ScrollReveal().reveal('.fadeIn', {
+ScrollReveal().reveal(".work__images", {
   delay: 300, // アニメーション開始までの時間
   duration: 2000, // アニメーションの完了にかかる時間
   viewFactor: 0, // 0~1,どれくらい見えたら実行するか
-  origin: 'top', // 要素がどの方向から来るか
-  opacity: 0, //アニメーション開始時の透明度
-  reset:false   // 何回もアニメーション表示するか
+  origin: "right", // 要素がどの方向から来るか
+  distance: "50px", // 移動する距離
+  reset: false, // 何回もアニメーション表示するか
+});
+ScrollReveal().reveal(".about__images", {
+  delay: 300, // アニメーション開始までの時間
+  duration: 2000, // アニメーションの完了にかかる時間
+  viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+  origin: "left", // 要素がどの方向から来るか
+  distance: "50px", // 移動する距離
+  reset: false, // 何回もアニメーション表示するか
+});
+ScrollReveal().reveal(".staff__images-right", {
+  delay: 300, // アニメーション開始までの時間
+  duration: 2000, // アニメーションの完了にかかる時間
+  viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+  origin: "right", // 要素がどの方向から来るか
+  distance: "50px", // 移動する距離
+  reset: false, // 何回もアニメーション表示するか
+});
+ScrollReveal().reveal(".staff__images-left", {
+  delay: 300, // アニメーション開始までの時間
+  duration: 2000, // アニメーションの完了にかかる時間
+  viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+  origin: "left", // 要素がどの方向から来るか
+  distance: "50px", // 移動する距離
+  reset: false, // 何回もアニメーション表示するか
 });
 
-ScrollReveal().reveal('.work__images', {
-  delay: 300, // アニメーション開始までの時間
-  duration: 2000, // アニメーションの完了にかかる時間
-  viewFactor: 0, // 0~1,どれくらい見えたら実行するか
-  origin: 'right', // 要素がどの方向から来るか
-  distance: '50px', // 移動する距離
-  reset:false   // 何回もアニメーション表示するか
-});
-ScrollReveal().reveal('.about__images', {
-  delay: 300, // アニメーション開始までの時間
-  duration: 2000, // アニメーションの完了にかかる時間
-  viewFactor: 0, // 0~1,どれくらい見えたら実行するか
-  origin: 'left', // 要素がどの方向から来るか
-  distance: '50px', // 移動する距離
-  reset:false   // 何回もアニメーション表示するか
-});
-ScrollReveal().reveal('.staff__images-right', {
-  delay: 300, // アニメーション開始までの時間
-  duration: 2000, // アニメーションの完了にかかる時間
-  viewFactor: 0, // 0~1,どれくらい見えたら実行するか
-  origin: 'right', // 要素がどの方向から来るか
-  distance: '50px', // 移動する距離
-  reset:false   // 何回もアニメーション表示するか
-});
-ScrollReveal().reveal('.staff__images-left', {
-  delay: 300, // アニメーション開始までの時間
-  duration: 2000, // アニメーションの完了にかかる時間
-  viewFactor: 0, // 0~1,どれくらい見えたら実行するか
-  origin: 'left', // 要素がどの方向から来るか
-  distance: '50px', // 移動する距離
-  reset:false   // 何回もアニメーション表示するか
-});
-
-ScrollReveal().reveal('.work__person01', {
+ScrollReveal().reveal(".work__person01", {
   delay: 300, // アニメーション開始までの時間
   duration: 1000, // アニメーション完了にかかる時間
-  origin: 'top', // 要素がどの方向から来るか
-  distance: '50px', // 移動する距離
-  reset: false // フレームインの度に動かすか
+  origin: "top", // 要素がどの方向から来るか
+  distance: "50px", // 移動する距離
+  reset: false, // フレームインの度に動かすか
 });
-ScrollReveal().reveal('.work__person02', {
+ScrollReveal().reveal(".work__person02", {
   delay: 300, // アニメーション開始までの時間
   duration: 1000, // アニメーション完了にかかる時間
-  origin: 'top', // 要素がどの方向から来るか
-  distance: '50px', // 移動する距離
-  reset: false // フレームインの度に動かすか
+  origin: "top", // 要素がどの方向から来るか
+  distance: "50px", // 移動する距離
+  reset: false, // フレームインの度に動かすか
 });
-ScrollReveal().reveal('.about__person03', {
+ScrollReveal().reveal(".about__person03", {
   delay: 300, // アニメーション開始までの時間
   duration: 1000, // アニメーション完了にかかる時間
-  origin: 'top', // 要素がどの方向から来るか
-  distance: '50px', // 移動する距離
-  reset: false // フレームインの度に動かすか
+  origin: "top", // 要素がどの方向から来るか
+  distance: "50px", // 移動する距離
+  reset: false, // フレームインの度に動かすか
 });
-ScrollReveal().reveal('.staff__person04', {
+ScrollReveal().reveal(".staff__person04", {
   delay: 200, // アニメーション開始までの時間
   duration: 1000, // アニメーション完了にかかる時間
-  origin: 'top', // 要素がどの方向から来るか
-  distance: '70px', // 移動する距離
-  reset: false // フレームインの度に動かすか
+  origin: "top", // 要素がどの方向から来るか
+  distance: "70px", // 移動する距離
+  reset: false, // フレームインの度に動かすか
 });
-
-
 
 
 // アニメーションさせたいクラス
-var container = $(".hero__title");
+var container = $(".loading__title");
 // アニメーションスピード
 var speed = 80;
 
@@ -205,8 +196,8 @@ var text = $.trim(content);
 var newHtml = "";
 
 // スペースで区切ったテキストを、テキストの数だけspanで囲む
-text.split("").forEach(function(v) {
- newHtml += '<span>' + v + '</span>';
+text.split("").forEach(function (v) {
+  newHtml += "<span>" + v + "</span>";
 });
 
 // spanで囲んだテキスト群をHTMLに戻す
@@ -214,83 +205,68 @@ container.html(newHtml);
 
 // 1文字ずつ表示
 var txtNum = 0;
-container.css({opacity: 1});
-setInterval(function() {
-  container.find('span').eq(txtNum).css({opacity: 1});
- txtNum++
+container.css({ opacity: 1 });
+setInterval(function () {
+  container.find("span").eq(txtNum).css({ opacity: 1 });
+  txtNum++;
 }, speed);
 
-
-
-// フッタートップスクロール
-// $(function($){
-
-//   var sideBar = $('.footer__scroll > a');
-//   sideBar.hide();
-  
-//   $(window).scroll(function () {
-//       if ($(this).scrollTop() > 100) {
-//         sideBar.fadeIn();
-//       } else {
-//         sideBar.fadeOut();
-//       }
-//   });
-// });
-
-$(function(){
-  var pagetop = $('.footer__scroll > a');
+$(function () {
+  var pagetop = $(".footer__scroll > a");
   // ボタン非表示
   pagetop.hide();
   // 100px スクロールしたらボタン表示
   $(window).scroll(function () {
-     if ($(this).scrollTop() > 100) {
-          pagetop.fadeIn();
-     } else {
-          pagetop.fadeOut();
-     }
+    if ($(this).scrollTop() > 100) {
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
   });
   pagetop.click(function () {
-     $('body, html').animate({ scrollTop: 0 }, 500);
-     return false;
+    $("body, html").animate({ scrollTop: 0 }, 500);
+    return false;
   });
 });
 
 
-//logoの表示
-$(window).on('load',function(){
-  $(".loading").delay(1800).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
- 
+// logoの表示
+$(window).on("load", function () {
+  $(".loading").delay(1600).fadeOut("200");
+  
 });
-
 
 
 // アーチ型ヒーロータイトル
-$(function() {
+$(function () {
   $(".hero__title").arctext({
     radius: 160,
-    dir: 1,
-    rotate: true,
+  });
+  
+  $(".loading__title").arctext({
+    radius: 160,
   });
 });
 
-
 // ロゴのフェイドイン
-$(function() {
-	setTimeout(function(){
-		$('.hero__title-logo, .hero__title-logo2').fadeIn(1800);
-	},400); //1.9秒後にロゴをフェードイン
-	
+$(function () {
+  setTimeout(function () {
+    $(".hero__title, .hero__title-logo, .hero__title-logo2, .hero__title-logo3").fadeIn(800);
+  }, 2300); //1.9秒後にロゴをフェードイン
 });
 
 
-$(function() {
-	setTimeout(function(){
-		$('.hero__title-logo3').fadeIn(500);
-	},2200); //1.9秒後にロゴをフェードイン
-	
+$('.loading__title01').fadeIn(1500);
+$('.loading__title02').fadeIn(1500);
+
+
+// パララックス
+new BackgroundVideo('.bv-video', {
+  src: [
+      //動画保存先のパス
+      './images/top-movie.mp4'
+  ]
 });
-
-
 
 
 
